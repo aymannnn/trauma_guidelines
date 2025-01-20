00c815046897ab10a167b1a30038100959a39a1c
aymannnn <ayman.ali1230@gmail.com>
2025-01-20 07:22:16 -0500

    still broken but attempt to fix paths

--- scripts/generate_pages_from_csv.py
+++ scripts/generate_pages_from_csv.py
@@ -31,6 +31,7 @@ category_template = """<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{title}</title>
+    <base href = "/trauma_guidelines/">
     <link rel="stylesheet" href="/styles.css">
 </head>
 
