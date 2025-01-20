ce70e1f8a223605d19ce547192bea717c614c8cc
aymannnn <ayman.ali1230@gmail.com>
2025-01-19 21:56:55 -0500

    removed bullet poitn formats

--- scripts/generate_pages_from_csv.py
+++ scripts/generate_pages_from_csv.py
@@ -61,33 +61,33 @@ for filename, title in categories:
         """
     elif filename == "outside_resources":
         content = """
-        <ul class="button-container">
-            <li><button onclick="location.href='https://www.east.org/education-resources/practice-management-guidelines/category/trauma'">EAST PMG</button></li>
-            <li><button onclick="location.href='https://www.westerntrauma.org/algorithms.php'">WTA Algorithms</button></li>
-            <li><button onclick="location.href='https://www.surgicalcriticalcare.net/guidelines.php'">ORMC Surgical Critical Care Guidelines</button></li>
-            <li><button onclick="location.href='https://app.behindtheknife.org/home'">Behind the Knife</button></li>
-            <li><button onclick="location.href='https://litfl.com/'">Life in the Fast Lane</button></li>
-            <li><button onclick="location.href='https://emcrit.org/category/pulmcrit/'">PulmCrit/EMCrit</button></li>
-            <li><button onclick="location.href='https://www.thebottomline.org.uk/'">The Bottom Line</button></li>
-            <li><button onclick="location.href='https://rebelem.com/'">RebelEM</button></li>
-            <li><button onclick="location.href='https://thetraumapro.com/'">The Trauma Pro</button></li>
-            <li><button onclick="location.href='https://intensivecarenetwork.com/'">Intensive Care Network</button></li>
-            <li><button onclick="location.href='https://criticalcarereviews.com/index.php'">Critical Care Reviews</button></li>
-            <li><button onclick="location.href='https://www.wikijournalclub.org/wiki/Main_Page'">Wiki Journal Club</button></li>
-            <li><button onclick="location.href='https://www.orthobullets.com'">Ortho Bullets</button></li>
-        </ul>
+        <div class="button-container">
+            <button onclick="location.href='https://www.east.org/education-resources/practice-management-guidelines/category/trauma'">EAST PMG</button>
+            <button onclick="location.href='https://www.westerntrauma.org/algorithms.php'">WTA Algorithms</button>
+            <button onclick="location.href='https://www.surgicalcriticalcare.net/guidelines.php'">ORMC Surgical Critical Care Guidelines</button>
+            <button onclick="location.href='https://app.behindtheknife.org/home'">Behind the Knife</button>
+            <button onclick="location.href='https://litfl.com/'">Life in the Fast Lane</button>
+            <button onclick="location.href='https://emcrit.org/category/pulmcrit/'">PulmCrit/EMCrit</button>
+            <button onclick="location.href='https://www.thebottomline.org.uk/'">The Bottom Line</button>
+            <button onclick="location.href='https://rebelem.com/'">RebelEM</button>
+            <button onclick="location.href='https://thetraumapro.com/'">The Trauma Pro</button>
+            <button onclick="location.href='https://intensivecarenetwork.com/'">Intensive Care Network</button>
+            <button onclick="location.href='https://criticalcarereviews.com/index.php'">Critical Care Reviews</button>
+            <button onclick="location.href='https://www.wikijournalclub.org/wiki/Main_Page'">Wiki Journal Club</button>
+            <button onclick="location.href='https://www.orthobullets.com'">Ortho Bullets</button>
+        </div>
         """
     else:
         csv_path = f"docs/csvs/{filename}.csv"
-        content = "<ul>\n"
+        content = "<div>\n"
         if os.path.exists(csv_path):
             with open(csv_path, newline='') as csvfile:
                 reader = csv.reader(csvfile)
                 for row in reader:
                     text, pdf_path = row
                     pdf_path = '../test_pdfs/' + pdf_path
-                    content += f'    <li><a href="{pdf_path}">{text}</a></li>\n'
-        content += "</ul>"
+                    content += f'    <div><a href="{pdf_path}">{text}</a></div>\n'
+        content += "</div>"
     with open(f"../docs/pages/{filename}.html", "w") as file:
         file.write(category_template.format(title=title, content=content))
 
